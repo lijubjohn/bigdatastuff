f6195eef71cbc24732272cc4a4b0cefa21cd6298
Liju John <liju@apple.com>
2017-02-14 22:36:24 -0800

    updated pom

---
+++ hadoop-distcp/src/main/java/CopyFile.java
@@ -0,0 +1,19 @@
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.tools.DistCp;
+
+/**
+ * Created by liju on 2/14/17.
+ */
+public class CopyFile {
+
+    public static void main(String[] args) {
+        Configuration conf = new Configuration();
+
+        String sourceNameNode = "";
+        String destinationNameNode = "";
+
+        DistCp distCp = new DistCp();
+
+    }
+
+}
